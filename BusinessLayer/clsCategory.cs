91ab5eeb275d6962bf7616089758dfa0657a45f8
Goldyzh <g2tho2@gmail.com>
2026-01-19 00:52:17 +0300

    Category class for data and business

---
+++ BusinessLayer/clsCategory.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Data;
+using System.Xml.Linq;
+using Restaurant_DataAccess;
+
+
+
+namespace Restaurant_Buisness
+{
+    public  class clsCategory
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int CategoryID { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+
+        private string _ImagePath;
+      
+        public string ImagePath   
+        {
+            get { return _ImagePath; }   
+            set { _ImagePath = value; }  
+        }
+
+        public clsCategory()
+
+        {
+            this.CategoryID = -1;
+            this.Name = "";
+            this.Description = "";
+            this.ImagePath = "";
+
+            Mode = enMode.AddNew;
+        }
+
+        private clsCategory(int CategoryID, string Name,string Description, string ImagePath)
+
+        {
+            this.CategoryID = CategoryID;
+            this.Name = Name;
+            this.Description= Description;
+            this.ImagePath = ImagePath;
+            Mode = enMode.Update;
+        }
+
+        private bool _AddNewCategory()
+        {
+            //call DataAccess Layer 
+
+            this.CategoryID = clsCategoryData.AddNewCategory(
+                this.Name,this.Description , this.ImagePath);
+
+            return (this.CategoryID != -1);
+        }
+
+        private bool _UpdateCategory()
+        {
+            //call DataAccess Layer 
+
+            return clsCategoryData.UpdateCategory(
+                this.CategoryID, this.Name,this.Description, this.ImagePath);
+        }
+
+        public static clsCategory Find(int CategoryID)
+        {
+
+            string Name = "", Description = "",  ImagePath = "";            
+
+            bool IsFound = clsCategoryData.GetCategoryInfoByID 
+                                (
+                                    CategoryID, ref Name, ref Description, ref ImagePath
+                                );
+
+            if (IsFound)
+                //we return new object of that Category with the right data
+                return new clsCategory(CategoryID, Name,Description , ImagePath);
+            else
+                return null;
+        }
+
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewCategory())
+                    {
+
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+
+                    return _UpdateCategory();
+
+            }
+
+            return false;
+        }
+
+        public static DataTable GetAllPeople()
+        {
+            return clsCategoryData.GetAllCategories();
+        }
+
+        public static bool DeleteCategory(int ID)
+        {
+            return clsCategoryData.DeleteCategory(ID); 
+        }
+
+        public static bool isCategoryExist(int ID)
+        {
+           return clsCategoryData.IsCategoryExist(ID);
+        }
+
+
+    }
+}
---
+++ RestaurantDataAccessLayer/clsCategoryData.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Restaurant_DataAccess
+{
+    public class clsCategoryData
+    {
+       
+        public static bool GetCategoryInfoByID(int CategoryID, ref string Name, ref string Description, ref string ImagePath)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT * FROM Categories WHERE CategoryID = @CategoryID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@CategoryID", CategoryID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    // The record was found
+                    isFound = true;
+
+                    Name = (string)reader["Name"];
+                    Description = (string)reader["Description"];
+
+
+                    //ImagePath: allows null in database so we should handle null
+                    if (reader["ImagePath"] != DBNull.Value)
+                    {
+                        ImagePath = (string)reader["ImagePath"];
+                    }
+                    else
+                    {
+                        ImagePath = "";
+                    }
+
+                }
+                else
+                {
+                    // The record was not found
+                    isFound = false;
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+
+
+      
+
+
+        public static int AddNewCategory( string Name,  string Description, string ImagePath)
+        {
+            //this function will return the new Category id if succeeded and -1 if not.
+            int CategoryID = -1;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"INSERT INTO Categories (Name, Description,ImagePath)
+                             VALUES (@Name, @Description@ImagePath);
+                             SELECT SCOPE_IDENTITY();";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@Name", Name);
+            command.Parameters.AddWithValue("@Description", Description);
+           
+
+            if (ImagePath != "" && ImagePath != null)
+                command.Parameters.AddWithValue("@ImagePath", ImagePath);
+            else
+                command.Parameters.AddWithValue("@ImagePath", System.DBNull.Value);
+
+            try
+            {
+                connection.Open();
+
+                object result = command.ExecuteScalar();
+
+                if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                {
+                    CategoryID = insertedID;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+            return CategoryID;
+        }
+
+
+
+        public static bool UpdateCategory(int CategoryID,  string Name, string Description, string ImagePath)
+        {
+
+            int rowsAffected = 0;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"Update  Categories  
+                            set Name = @Name,
+                                Description = @Description,
+                                ImagePath =@ImagePath
+                                where CategoryID = @CategoryID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@CategoryID", CategoryID);
+            command.Parameters.AddWithValue("@Name", Name);
+            command.Parameters.AddWithValue("@Description", Description);
+
+            if (ImagePath != "" && ImagePath != null)
+                command.Parameters.AddWithValue("@ImagePath", ImagePath);
+            else
+                command.Parameters.AddWithValue("@ImagePath", System.DBNull.Value);
+
+
+            try
+            {
+                connection.Open();
+                rowsAffected = command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                return false;
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+            return (rowsAffected > 0);
+        }
+
+
+        public static DataTable GetAllCategories()
+        {
+
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"SELECT * FROM Categories";
+
+
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+
+                {
+                    dt.Load(reader);
+                }
+
+                reader.Close();
+
+
+            }
+
+            catch (Exception ex)
+            {
+                // Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dt;
+
+        }
+
+        public static bool DeleteCategory(int CategoryID)
+        {
+
+            int rowsAffected = 0;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"Delete Categories 
+                                where CategoryID = @CategoryID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@CategoryID", CategoryID);
+
+            try
+            {
+                connection.Open();
+
+                rowsAffected = command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                // Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+
+                connection.Close();
+
+            }
+
+            return (rowsAffected > 0);
+
+        }
+
+        public static bool IsCategoryExist(int CategoryID)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT Found=1 FROM Categories WHERE CategoryID = @CategoryID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@CategoryID", CategoryID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                isFound = reader.HasRows;
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+
+
+    }
+}
