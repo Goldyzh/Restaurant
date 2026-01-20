15fe5bceaccf3fa6b1a99f3df5bbff00cded5646
Goldyzh <g2tho2@gmail.com>
2026-01-20 22:02:05 +0300

    clsOrderItemsData

---
+++ RestaurantDataAccessLayer/clsOrderItemsData.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Restaurant_DataAccess
+{
+    public class clsOrderItemsItemsData
+    {
+        
+
+        public static bool GetOrderItemsInfoByID(int ID, ref int OrderID, ref int ItemID)
+            {
+                bool isFound = false;
+
+                SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+                string query = "SELECT * FROM OrderItems WHERE ID = @ID";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@ID", ID);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+
+                        // The record was found
+                        isFound = true;
+
+                        ID = (int)reader["ID"];
+                        OrderID = (int)reader["OrderID"];
+                        ItemID = (int)reader["ItemID"];
+
+
+                }
+                else
+                    {
+                        // The record was not found
+                        isFound = false;
+                    }
+
+                    reader.Close();
+
+
+                }
+                catch (Exception ex)
+                {
+                    //Console.WriteLine("Error: " + ex.Message);
+                    isFound = false;
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+                return isFound;
+            }
+
+        public static DataTable GetOrderItems()
+            {
+
+                DataTable dt = new DataTable();
+                SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+                  string query = "SELECT * FROM OrderItems Order BY ID DESC";
+
+
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+                try
+                {
+                    connection.Open();
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+
+                    {
+                        dt.Load(reader);
+                    }
+
+                    reader.Close();
+
+
+                }
+
+                catch (Exception ex)
+                {
+                    // Console.WriteLine("Error: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+                return dt;
+
+            }
+
+
+
+
+
+        public static int AddNewOrderItems(int OrderID,  int ItemID)
+        {
+
+            //this function will return the new person id if succeeded and -1 if not.
+            int ID = -1;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"INSERT INTO OrderItems ( 
+                            OrderID,ItemID)
+                             VALUES (@OrderID,@ItemID);
+                             SELECT SCOPE_IDENTITY();";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("OrderID", @OrderID);
+            command.Parameters.AddWithValue("ItemID", @ItemID);
+
+
+
+            try
+            {
+                connection.Open();
+
+                object result = command.ExecuteScalar();
+
+                if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                {
+                    ID = insertedID;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+
+            return ID;
+        }
+
+
+        public static bool UpdateOrderItems(int ID, int OrderID, int ItemID)
+        {
+
+            int rowsAffected = 0;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"Update  OrderItems  
+                            set OrderID = @OrderID,
+                                ItemID = @ItemID
+                            where ID=@ID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@ID", ID);
+            command.Parameters.AddWithValue("OrderID", OrderID);
+            command.Parameters.AddWithValue("ItemID", ItemID);
+
+
+
+
+            try
+            {
+                connection.Open();
+                rowsAffected = command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                return false;
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+            return (rowsAffected > 0);
+        }
+
+        public static bool DeleteOrderItems(int ID)
+        {
+
+            int rowsAffected = 0;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"Delete OrderItems 
+                                where ID = @ID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@ID", ID);
+
+            try
+            {
+                connection.Open();
+
+                rowsAffected = command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                // Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+
+                connection.Close();
+
+            }
+
+            return (rowsAffected > 0);
+
+        }
+
+        public static bool IsOrderItemsExist(int ID)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT Found=1 FROM OrderItems WHERE ID = @ID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@ID", ID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                isFound = reader.HasRows;
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+
+  
+
+     
+
+      
+      
+
+    }
+}
